860dc5e332e2ac88f2ac12a2d765ca2f02101a80
Maxime Poulain <maxime.poulain@outlook.be>
2016-09-25 01:12:21 +0200

    import dtbtool and imported buildkernel.sh

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export ARCH=arm64;
-export CROSS_COMPILE="ccache /home/khaon/android/linaro-64/bin/aarch64-none-elf-";
+export CROSS_COMPILE="ccache /home/khaon/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-";
 
 export mkboot_root_folder=/home/khaon/android/mkbootimg_tools;
-export mkboot_project_name=orig-mm;
+export mkboot_project_name=mm-boot;
 export boot_image_name=boot-mm.img;
 
 make mrproper;
@@ -12,8 +12,10 @@ make a3xelte_00_defconfig;
 
 make -j9;
 
+./dtbtool  -o arch/arm64/boot/dt.img -s 2048 -p scripts/dtc/ arch/arm64/boot/dts/ -v;
 
 cp arch/arm64/boot/Image $mkboot_root_folder/$mkboot_project_name/kernel;
+cp arch/arm64/boot/dt.img $mkboot_root_folder/$mkboot_project_name/dt.img;
 cd $mkboot_root_folder;
 rm $boot_image_name;
 ./mkboot $mkboot_project_name $boot_image_name;
